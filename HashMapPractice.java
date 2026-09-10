1706fd6d4f15cc038cfcc8eb488724ce1af84131
SRI RANGANAATH <sriranganaath1207@gmail.com>
2026-09-10 12:11:52 +0530

    Finding largest frequency added

--- HashMapPractice.java
+++ HashMapPractice.java
@@ -1,8 +1,33 @@
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.Map;
 public class HashMapPractice {
     public static void main(String[] args){
         //1. Checking whether an element exists
+
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+
+        // int[] arr = new int[n];
+        // for(int i=0;i<n;i++){
+        //     arr[i] = sc.nextInt();
+        // }
+        // int target = sc.nextInt();
+
+        // HashMap<Integer,Integer> mpp = new HashMap<>();
+        // for(int i=0;i<n;i++){
+        //     if(!mpp.containsKey(arr[i]))
+        //     mpp.put(arr[i],i);
+        // }
+        // if(mpp.containsKey(target)){
+        //     System.out.println("Found at index:" +mpp.get(target));
+        // }else{
+        //     System.out.println("Not found");
+        // }
+
+
+        //2. Finding number with highest frequency
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
@@ -10,17 +35,21 @@ public class HashMapPractice {
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-        int target = sc.nextInt();
-
+        int max=0,element=0;
         HashMap<Integer,Integer> mpp = new HashMap<>();
         for(int i=0;i<n;i++){
-            if(!mpp.containsKey(arr[i]))
-            mpp.put(arr[i],i);
+            mpp.put(arr[i],mpp.getOrDefault(arr[i],0)+1); //returns current frequency if found and 0 if not and increments by 1
         }
-        if(mpp.containsKey(target)){
-            System.out.println("Found at index:" +mpp.get(target));
-        }else{
-            System.out.println("Not found");
+        int currentFrequency = 0,currentElement = 0;
+        for(Map.Entry<Integer,Integer> entry: mpp.entrySet()){
+            // if(entry.values())
+            currentFrequency = entry.getValue();
+            currentElement = entry.getKey();
+            if(currentFrequency > max){
+                max = currentFrequency;
+                element = currentElement;
+            }
         }
+        System.out.println(element);
     }
 }
