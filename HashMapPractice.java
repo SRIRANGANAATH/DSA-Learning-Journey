1d09127a9101cb4a784207354444386c28e9f9bc
SRI RANGANAATH <sriranganaath1207@gmail.com>
2026-09-10 14:30:07 +0530

    finding lowest frequency and finding distict No. of  elements included

--- HashMapPractice.java
+++ HashMapPractice.java
@@ -27,7 +27,38 @@ public class HashMapPractice {
 
 
         //2. Finding number with highest frequency
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
 
+        // int[] arr = new int[n];
+        // for(int i=0;i<n;i++){
+        //     arr[i] = sc.nextInt();
+        // }
+        // int max=0,maxElement=0;
+        // int low=0,lowElement=0;
+        // HashMap<Integer,Integer> mpp = new HashMap<>();
+        // for(int i=0;i<n;i++){
+        //     mpp.put(arr[i],mpp.getOrDefault(arr[i],0)+1); //returns current frequency if found and 0 if not and increments by 1
+        // }
+        // int currentFrequency = 0,currentElement = 0;
+        // for(Map.Entry<Integer,Integer> entry: mpp.entrySet()){
+        //     // if(entry.values())
+        //     currentFrequency = entry.getValue();
+        //     currentElement = entry.getKey();
+        //     if(currentFrequency > max){
+        //         max = currentFrequency;
+        //         maxElement = currentElement;
+        //     }
+        //     if(max > currentFrequency){
+        //         low = currentFrequency;
+        //         lowElement = currentElement;
+        //     }
+        // }
+        // System.out.println(maxElement+" : "+max);
+        // System.out.println(lowElement+" : "+low);
+
+
+        //3. Count distinct Elements
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
@@ -35,21 +66,18 @@ public class HashMapPractice {
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-        int max=0,element=0;
+        int count = 0;
         HashMap<Integer,Integer> mpp = new HashMap<>();
         for(int i=0;i<n;i++){
-            mpp.put(arr[i],mpp.getOrDefault(arr[i],0)+1); //returns current frequency if found and 0 if not and increments by 1
+            mpp.put(arr[i],mpp.getOrDefault(arr[i],0)+1);
         }
-        int currentFrequency = 0,currentElement = 0;
-        for(Map.Entry<Integer,Integer> entry: mpp.entrySet()){
-            // if(entry.values())
-            currentFrequency = entry.getValue();
-            currentElement = entry.getKey();
-            if(currentFrequency > max){
-                max = currentFrequency;
-                element = currentElement;
-            }
-        }
-        System.out.println(element);
+        // Method - 1
+        // for(Map.Entry<Integer,Integer> entry: mpp.entrySet()){
+        //         count++;
+        // }
+        // System.out.println(count);
+
+        //Method - 2 (in-built method)
+        System.out.println(mpp.size());
     }
 }
